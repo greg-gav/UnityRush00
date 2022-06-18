f23caabd9210b362460dda56d6d97cdafdc05533
gvarys <johnnyzaev@icloud.com>
2022-06-18 14:23:54 +0300

    WeaponSystem prototype done!

---
+++ Assets/Scripts/PickableWeapon.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D), typeof(SpriteRenderer))]
+public class PickableWeapon : MonoBehaviour
+{
+	[SerializeField] private WeaponBase weaponBase;
+	private CircleCollider2D _weaponCollider;
+	private SpriteRenderer _weaponSprite;
+
+	private void Awake()
+	{
+		_weaponCollider = GetComponent<CircleCollider2D>();
+		_weaponSprite = GetComponent<SpriteRenderer>();
+		_weaponSprite.sprite = weaponBase.icon;
+		_weaponCollider.isTrigger = true;
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+			Debug.Log("PickWeapon");
+	}
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+			Debug.Log("PickWeapon");
+	}
+}
